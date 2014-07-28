2de2da14a5a4c9ee35dcf80d33f00a4d84efbd9c
John Lozano <john.lozano@rackspace.com>
2014-07-28 18:53:29 -0500

    controller tests

---
+++ test/controllers/status.js
@@ -0,0 +1,65 @@
+var moment = require('moment');
+var statusController = require('../../lib/controllers/status');
+var mongoose = require('mongoose');
+var statusModel = require('../../lib/models/status');
+var stubbedModel = { '_id': 0, 'userId': 0, 'status': 'status' };
+var statusStub;
+
+describe('Status Controller', function () {
+
+    beforeEach(function () {
+    });
+
+    afterEach(function () {
+        statusStub.restore();
+    });
+
+    it('should create a status', function () {
+        statusStub = sinon.stub(statusModel, 'create').returns({
+            save: sinon.stub().yields(null, stubbedModel)
+        });
+        var req = {};
+        req.body = {
+            username: 'testuser' + moment().valueOf(),
+            status: 'test status' + moment().valueOf()
+        };
+
+        var res = { send: sinon.spy() };
+        statusController.create(req, res);
+        expect(statusStub).to.be.calledWith(req.body);
+        expect(res.send).to.be.called;
+    });
+
+    it('should list all status', function () {
+        statusStub = sinon.stub(statusModel, 'find').yields(null, stubbedModel);
+        var req = {};
+        req.body = {
+            username: 'testuser' + moment().valueOf(),
+            status: 'test status' + moment().valueOf()
+        };
+
+        var res = { send: sinon.spy() };
+        statusController.listAllStatuses(req, res);
+        expect(statusStub).to.be.calledOnce;
+        expect(res.send).to.be.calledWith(stubbedModel);
+    });
+
+    it('should modify a status', function () {
+        var updateStub = { update: sinon.spy() } 
+        statusStub = sinon.stub(statusModel, 'findOne').yields(null, updateStub);
+        var req = {};
+        req.body = {
+            username: 'testuser' + moment().valueOf(),
+            status: 'test status' + moment().valueOf()
+        };
+
+        var res = { send: sinon.spy() };
+        statusController.listAllStatuses(req, res);
+        expect(statusStub).to.be.calledOnce;
+        expect(updateStub.update).to.be.calledOnce;
+        expect(res.send).to.be.calledWith({ status: "status updated" });
+      
+    });
+
+    it('should remove a status')
+});
